d38f1f5472a3409a6189644b5246cdda0f1244e4
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2020-07-08 18:30:16 -0400

    Enhancement: Code ready for v0

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
 $(function () {
-  // Read environment.
-  var env = JSON.parse(environment)
-
-  // Endpoints.
-  var host = env.host
-  var port = env.port
-  var backendEndpoint = '//' + host + ':' + port
-
   // Enable tooltips.
   $('[data-toggle="tooltip"]').tooltip()
 
@@ -14,15 +6,6 @@ $(function () {
   $('#coming-soon-tab').on('shown.bs.tab', function (event) {
     var tabId = $(event.target).attr('href')
     var $comingSoonElement = $(tabId)
-
-    var backendRequest = $.get(backendEndpoint + '/api/coming-soon/lead')
-      .done((data) => {
-        console.log(data);
-        $comingSoonElement.find('.lead').html(data)
-      })
-      .fail((error) => {
-        console.log(error);
-        $comingSoonElement.find('.lead').html('Data not available!')
-      })
+    $comingSoonElement.find('.lead').html('Tune in later to find out more useful stock advices.')
   })
 })
